5f04808c88d498868fb3709612db6e6ef0dc6d87
Truman Arneld <tarneld@oa-k3.msk.21-school.ru>
2020-11-04 20:51:16 +0300

    3try-nomap

--- ft_lstmap.c
+++ ft_lstmap.c
@@ -1,19 +1,39 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstdelone.c                                     :+:      :+:    :+:   */
+/*   ft_lstmap.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: tarneld <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/11/03 13:21:56 by tarneld           #+#    #+#             */
-/*   Updated: 2020/11/03 13:21:58 by tarneld          ###   ########.fr       */
+/*   Created: 2020/11/04 16:17:51 by tarneld           #+#    #+#             */
+/*   Updated: 2020/11/04 16:17:54 by tarneld          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void				ft_lstdelone(t_list *lst, void (*del)(void*))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	(*del)(lst->content);
-	free(lst);
+	t_list	*new;
+	t_list	*elem;
+
+	new = NULL;
+	if (!lst)
+		return (NULL);
+	if (!(new = ft_lstnew(f(lst->content))))
+		return (NULL);
+	while (lst)
+	{
+		if (lst->next)
+		{
+			if (!(elem = ft_lstnew(f(lst->next->content))))
+			{
+				ft_lstclear(&new, del);
+				return (NULL);
+			}
+			ft_lstadd_back(&new, elem);
+		}
+		lst = lst->next;
+	}
+	return (new);
 }
