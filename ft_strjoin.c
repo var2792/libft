84a9f80185683dd5b01f41ef93aaabf041befb80
Truman Arneld <tarneld@oa-n1.msk.21-school.ru>
2020-11-12 15:59:02 +0300

    join

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -18,7 +18,7 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	int		j;
 	char	*res;
 
-	if (!s1 || !s2)
+	if (!s1 && !s2)
 		return (NULL);
 	if (!(res = malloc(sizeof(char) * (ft_strlen(s1) + ft_strlen(s2) + 1))))
 		return (NULL);
